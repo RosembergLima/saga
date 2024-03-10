cb623427b4c8f2faf004e1853bc4cf43d37ba63b
Rosemberg Bezerra <rosemberg.bezerra-ext@jci.com>
2024-03-10 09:58:00 +0000

    creating project

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/config/exception/ExceptionDetail.java
@@ -0,0 +1,4 @@
+package br.com.microservices.orchestrated.orderservice.config.exception;
+
+public record ExceptionDetail(int status, String message) {
+}
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/config/exception/ExceptionDetail.java
@@ -0,0 +1,4 @@
+package br.com.microservices.orchestrated.productvalidationservice.config.exception;
+
+public record ExceptionDetail(int status, String message) {
+}
