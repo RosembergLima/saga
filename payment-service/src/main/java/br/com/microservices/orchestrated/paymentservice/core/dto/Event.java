1374c035aaa26341b0096f9ae33b6bf336ab45cc
Rosemberg Bezerra <rosemberg.bezerra-ext@jci.com>
2024-03-16 13:04:23 +0000

    creating service on payment service

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Event.java
@@ -1,13 +1,16 @@
 package br.com.microservices.orchestrated.paymentservice.core.dto;
 
-import br.com.microservices.orchestrated.productvalidationservice.core.enums.ESagaStatus;
+import br.com.microservices.orchestrated.paymentservice.core.enums.ESagaStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Data
 @NoArgsConstructor
@@ -24,5 +27,10 @@ public class Event {
     private List<History> eventHistory;
     private LocalDateTime createdAt;
 
+    public void addToHistory(History history){
+        if(isEmpty(eventHistory))
+            eventHistory = new ArrayList<>();
+        eventHistory.add(history);
+    }
 }
 
