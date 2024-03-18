d24a2cc46085730e6993abd2e08f69e178cffe5b
Rosemberg Bezerra <rosemberg.bezerra-ext@jci.com>
2024-03-18 20:09:33 +0000

    creating service on inventory service

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -1,6 +1,7 @@
 package br.com.microservices.orchestrated.inventoryservice.core.consumer;
 
 
+import br.com.microservices.orchestrated.inventoryservice.core.service.InventoryService;
 import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,25 +14,26 @@ import org.springframework.stereotype.Component;
 public class InventoryConsumer {
 
     private final JsonUtil jsonUtil;
+    private final InventoryService inventoryService;
 
     @KafkaListener(
         groupId = "${spring.kafka.consumer.group-id}",
-        topics = "${spring.kafka.topic.payment-success}")
+        topics = "${spring.kafka.topic.inventory-success}")
     public void consumeSuccessEvent(String payload){
-        log.info("Receiving success event {} from payment-success topic",
+        log.info("Receiving success event {} from inventory-success topic",
                 payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.updateInventory(event);
     }
 
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
-            topics = "${spring.kafka.topic.payment-fail}")
+            topics = "${spring.kafka.topic.inventory-fail}")
     public void consumeFailEvent(String payload){
-        log.info("Receiving rollback event {} from payment-fail topic",
+        log.info("Receiving rollback event {} from inventory-fail topic",
                 payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.rollbackInventory(event);
     }
 
 }
